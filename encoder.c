54427b8caaeb0c5ee23e467e8da5f762a0167803
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-05-12 22:11:34 +0200

    Added homing, added AS5600_ENCODER struct as a base for all operations

--- encoder.c
+++ encoder.c
@@ -1,12 +1,11 @@
 #include "encoder.h"
 
-float getAngle(i2c_inst_t *i2c, const uint8_t ADDRESS)
+float getRawAngle(i2c_inst_t *i2c, uint8_t ADDRESS)
 {
-    float value = 0;
-    int raw_value = 0;
+    float value;
+    int raw_value;
     uint8_t data = 0;
-    uint16_t highbyte = 0;
-    uint16_t lowbyte = 0;
+    uint16_t highbyte, lowbyte;
 
     i2c_write_blocking(i2c, ADDRESS, &raw_angle_low, 1, false);
     i2c_read_blocking(i2c, ADDRESS, &data, 1, false);
@@ -15,6 +14,27 @@ float getAngle(i2c_inst_t *i2c, const uint8_t ADDRESS)
     i2c_read_blocking(i2c, ADDRESS, &data, 1, false);
     highbyte = data << 8;
     raw_value = highbyte | lowbyte;
-    value = raw_value*coefficient;
+    value = (float)raw_value*coefficient;
     return value;
+}
+
+void homeEncoder(AS5600_ENCODER* encoder, float home_position, i2c_inst_t* i2c, uint8_t ENCODER_ADDRESS){
+    float angle = getRawAngle(i2c, ENCODER_ADDRESS);
+    while(angle > home_position + 0.1 || angle < home_position - 0.1){
+        printf("Angle: %f\tHoming...\n", angle);
+        angle = getRawAngle(i2c, ENCODER_ADDRESS);
+    }
+    encoder->position=0;
+    encoder->rawPosition=0;
+    encoder->offset= getRawAngle(i2c, ENCODER_ADDRESS);
+    printf("Successfully homed AS5600 magnetic encoder!\n");
+    printf("Offset: %f\n", encoder->offset);
+}
+
+void updatePosition(AS5600_ENCODER* encoder, i2c_inst_t* i2c, uint8_t ENCODER_ADDRESS){
+    encoder->rawPosition = getRawAngle(i2c, ENCODER_ADDRESS);
+    encoder->position = encoder->rawPosition - encoder->offset;
+    if(encoder->position < 0){
+        encoder->position += 360;
+    }
 }
--- encoder.h
+++ encoder.h
@@ -5,13 +5,23 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// Const
-static float coefficient = 0.087890625f;
+// Coefficient for converting 12-bit information(0-4095) about angle to degrees (0-360)
+static float coefficient = 360/4096.f;
 
 // Raw angle register map
 static const uint8_t raw_angle_low = 0x0D; // Raw angle (7:0)
 static const uint8_t raw_angle_high = 0x0C; // Raw angle (11:8)
 
-float getAngle(i2c_inst_t *i2c, const uint8_t ADDRESS);
+typedef struct{
+    float rawPosition;
+    float position;
+
+    float offset;
+
+} AS5600_ENCODER;
+
+float getRawAngle(i2c_inst_t *i2c, uint8_t ADDRESS);
+void homeEncoder(AS5600_ENCODER* encoder, float home_position, i2c_inst_t* i2c, uint8_t ENCODER_ADDRESS);
+void updatePosition(AS5600_ENCODER* encoder, i2c_inst_t* i2c, uint8_t ENCODER_ADDRESS);
 
 #endif
